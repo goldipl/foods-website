c8f3a42b9115adae3ac22ec0849e26adb9c6f99f
goldipl <mgodlewski92@gmail.com>
2024-10-25 12:07:54 +0200

    config next app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+
+const nextConfig = {
+  // Your Next.js configuration options here
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add other configurations as needed
+};
 
 export default nextConfig;
