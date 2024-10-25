613c542cef96234ff50d13dde0635462c58e6972
goldipl <mgodlewski92@gmail.com>
2024-10-25 14:48:14 +0200

    add welcome section

---
+++ src/components/main-page/WelcomeSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WelcomeSection = () => {
+  return (
+    <div id="welcome-section" className="welcome-section">
+      <div className="welcome-section-wrapper">
+        <h2>Sekcja powitalna</h2>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomeSection;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import Header from "@/components/common/Header";
-import Footer from "@/components/common/Footer";
 import React from "react";
 import "@/css/swiper-bundle.min.css";
 import "@/sass/main.scss";
-import Hero from "@/components/main-page/Hero";
 import Topbar from "@/components/common/Topbar";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+import Hero from "@/components/main-page/Hero";
+import WelcomeSection from "@/components/main-page/WelcomeSection";
 
 const index = () => {
   return (
@@ -15,6 +16,7 @@ const index = () => {
       </header>
       <main>
         <Hero />
+        <WelcomeSection />
       </main>
       <footer>
         <Footer />
