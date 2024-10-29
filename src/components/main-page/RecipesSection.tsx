0680a0d3cdd5e49b26aa2b2a340968fab22e4ecc
goldipl <mgodlewski92@gmail.com>
2024-10-29 21:39:49 +0100

    add more recipes slots and flex-wraping styles

--- src/components/main-page/RecipesSection.tsx
+++ src/components/main-page/RecipesSection.tsx
@@ -103,6 +103,68 @@ const RecipesSection = () => {
               </div>
             </div>
           </Link>
+          {/* Recipe slot */}
+          <Link
+            href="https://www.instagram.com/p/DBdvWQouPv2/"
+            className="listing-v1-slot"
+          >
+            <div className="listing-v1-slot__img">
+              <Image
+                src={recipeImg001}
+                alt="Makaron"
+                width={400}
+                height={500}
+              ></Image>
+            </div>
+            <div className="listing-v1-slot__text">
+              <span>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Laborum quam laboriosam in tempore veritatis perspiciatis quo
+                expedita officiis eaque esse recusandae aperiam maxime impedit,
+                deleniti sequi similique animi voluptate nesciunt.
+              </span>
+            </div>
+            <div className="listing-v1-slot__labels">
+              <span>Obiad</span>
+            </div>
+            <div className="listing-v1-slot__zoom">
+              <div className="zoom-box">
+                <CiInstagram />
+                <span>Zobacz</span>
+              </div>
+            </div>
+          </Link>
+          {/* Recipe slot */}
+          <Link
+            href="https://www.instagram.com/p/DBdvWQouPv2/"
+            className="listing-v1-slot"
+          >
+            <div className="listing-v1-slot__img">
+              <Image
+                src={recipeImg001}
+                alt="Makaron"
+                width={400}
+                height={500}
+              ></Image>
+            </div>
+            <div className="listing-v1-slot__text">
+              <span>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Laborum quam laboriosam in tempore veritatis perspiciatis quo
+                expedita officiis eaque esse recusandae aperiam maxime impedit,
+                deleniti sequi similique animi voluptate nesciunt.
+              </span>
+            </div>
+            <div className="listing-v1-slot__labels">
+              <span>Obiad</span>
+            </div>
+            <div className="listing-v1-slot__zoom">
+              <div className="zoom-box">
+                <CiInstagram />
+                <span>Zobacz</span>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
