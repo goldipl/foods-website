bdaf7b0e2b5cdeb34ea922fd729e62b7cad66423
goldipl <mgodlewski92@gmail.com>
2024-10-29 17:52:35 +0100

    add boxes info content and styles

--- src/components/main-page/BoxesInfoSection.tsx
+++ src/components/main-page/BoxesInfoSection.tsx
@@ -1,10 +1,42 @@
 import React from "react";
+import { MdDoNotDisturb } from "react-icons/md";
+import { LuMilkOff } from "react-icons/lu";
+import { PiGrainsSlash } from "react-icons/pi";
 
 const BoxesInfoSection = () => {
   return (
     <div id="boxes-info-section" className="boxes-info-section">
       <div className="boxes-info-wrapper">
-        <h2>Znajdziesz u mnie, potrawy:</h2>
+        <h2>Znajdziesz u mnie, potrawy i przepisy:</h2>
+        <div className="boxes-info">
+          {/* Slot */}
+          <div className="boxes-info-slot">
+            <div className="boxes-info-slot__icon">
+              <PiGrainsSlash />
+            </div>
+            <div className="boxes-info-slot__text">
+              <span>bez glutenu</span>
+            </div>
+          </div>
+          {/* Slot */}
+          <div className="boxes-info-slot">
+            <div className="boxes-info-slot__icon">
+              <LuMilkOff />
+            </div>
+            <div className="boxes-info-slot__text">
+              <span>bez nabiału</span>
+            </div>
+          </div>
+          {/* Slot */}
+          <div className="boxes-info-slot">
+            <div className="boxes-info-slot__icon">
+              <MdDoNotDisturb />
+            </div>
+            <div className="boxes-info-slot__text">
+              <span>bez cukru</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
