4d392b075114cef5930d930753e3d00315d3c1c7
goldipl <mgodlewski92@gmail.com>
2024-10-30 08:00:43 +0100

    refactor and extract latest recipes data to seperate file

--- src/components/main-page/recipes/LatestRecipes.tsx
+++ src/components/main-page/recipes/LatestRecipes.tsx
@@ -1,176 +1,43 @@
 import Image from "next/image";
 import React from "react";
-import recipeImg001 from "./../../../../public/img/recipes/001_recipe.jpg";
-import recipeImg002 from "./../../../../public/img/recipes/002_recipe.jpg";
-import recipeImg003 from "./../../../../public/img/recipes/003_recipe.jpg";
 import Link from "next/link";
 import { CiInstagram } from "react-icons/ci";
+import { latestRecipesData } from "@/data/latest-recipes";
 
 const LatestRecipes = () => {
   return (
     <>
       <h2>Najnowsze przepisy</h2>
       <div className="listing-v1">
-        {/* Recipe slot */}
-        <Link
-          href="https://www.instagram.com/p/DBnv7v3OYgM/"
-          className="listing-v1-slot"
-          target="_blank"
-        >
-          <div className="listing-v1-slot__img">
-            <Image
-              src={recipeImg002}
-              alt="Makaron"
-              width={400}
-              height={500}
-            ></Image>
-          </div>
-          <div className="listing-v1-slot__text">
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              quam laboriosam in tempore veritatis perspiciatis quo expedita
-              officiis eaque esse recusandae aperiam maxime impedit, deleniti
-              sequi similique animi voluptate nesciunt.
-            </span>
-          </div>
-          <div className="listing-v1-slot__labels">
-            <span className="breakfast-label">Śniadanie</span>
-          </div>
-          <div className="listing-v1-slot__zoom">
-            <div className="zoom-box">
-              <CiInstagram />
-              <span>Zobacz</span>
+        {latestRecipesData.map((recipe) => (
+          <Link
+            key={recipe.id}
+            href={recipe.href}
+            className="listing-v1-slot"
+            target="_blank"
+          >
+            <div className="listing-v1-slot__img">
+              <Image
+                src={recipe.imgSrc}
+                alt={recipe.altText}
+                width={400}
+                height={500}
+              />
             </div>
-          </div>
-        </Link>
-        {/* Recipe slot */}
-        <Link
-          href="https://www.instagram.com/p/DBdvWQouPv2/"
-          className="listing-v1-slot"
-          target="_blank"
-        >
-          <div className="listing-v1-slot__img">
-            <Image
-              src={recipeImg001}
-              alt="Makaron"
-              width={400}
-              height={500}
-            ></Image>
-          </div>
-          <div className="listing-v1-slot__text">
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              quam laboriosam in tempore veritatis perspiciatis quo expedita
-              officiis eaque esse recusandae aperiam maxime impedit, deleniti
-              sequi similique animi voluptate nesciunt.
-            </span>
-          </div>
-          <div className="listing-v1-slot__labels">
-            <span className="dinner-label">Obiad</span>
-          </div>
-          <div className="listing-v1-slot__zoom">
-            <div className="zoom-box">
-              <CiInstagram />
-              <span>Zobacz</span>
+            <div className="listing-v1-slot__text">
+              <span>{recipe.description}</span>
             </div>
-          </div>
-        </Link>
-        {/* Recipe slot */}
-        <Link
-          href="https://www.instagram.com/p/DBrI559o2Eb/"
-          className="listing-v1-slot"
-          target="_blank"
-        >
-          <div className="listing-v1-slot__img">
-            <Image
-              src={recipeImg003}
-              alt="Makaron"
-              width={400}
-              height={500}
-            ></Image>
-          </div>
-          <div className="listing-v1-slot__text">
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              quam laboriosam in tempore veritatis perspiciatis quo expedita
-              officiis eaque esse recusandae aperiam maxime impedit, deleniti
-              sequi similique animi voluptate nesciunt.
-            </span>
-          </div>
-          <div className="listing-v1-slot__labels">
-            <span className="dessert-label">Deser</span>
-          </div>
-          <div className="listing-v1-slot__zoom">
-            <div className="zoom-box">
-              <CiInstagram />
-              <span>Zobacz</span>
+            <div className="listing-v1-slot__labels">
+              <span className={recipe.labelClass}>{recipe.label}</span>
             </div>
-          </div>
-        </Link>
-        {/* Recipe slot */}
-        <Link
-          href="https://www.instagram.com/p/DBdvWQouPv2/"
-          className="listing-v1-slot"
-          target="_blank"
-        >
-          <div className="listing-v1-slot__img">
-            <Image
-              src={recipeImg001}
-              alt="Makaron"
-              width={400}
-              height={500}
-            ></Image>
-          </div>
-          <div className="listing-v1-slot__text">
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              quam laboriosam in tempore veritatis perspiciatis quo expedita
-              officiis eaque esse recusandae aperiam maxime impedit, deleniti
-              sequi similique animi voluptate nesciunt.
-            </span>
-          </div>
-          <div className="listing-v1-slot__labels">
-            <span className="dinner-label">Obiad</span>
-          </div>
-          <div className="listing-v1-slot__zoom">
-            <div className="zoom-box">
-              <CiInstagram />
-              <span>Zobacz</span>
+            <div className="listing-v1-slot__zoom">
+              <div className="zoom-box">
+                <CiInstagram />
+                <span>Zobacz</span>
+              </div>
             </div>
-          </div>
-        </Link>
-        {/* Recipe slot */}
-        <Link
-          href="https://www.instagram.com/p/DBdvWQouPv2/"
-          className="listing-v1-slot"
-          target="_blank"
-        >
-          <div className="listing-v1-slot__img">
-            <Image
-              src={recipeImg001}
-              alt="Makaron"
-              width={400}
-              height={500}
-            ></Image>
-          </div>
-          <div className="listing-v1-slot__text">
-            <span>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
-              quam laboriosam in tempore veritatis perspiciatis quo expedita
-              officiis eaque esse recusandae aperiam maxime impedit, deleniti
-              sequi similique animi voluptate nesciunt.
-            </span>
-          </div>
-          <div className="listing-v1-slot__labels">
-            <span className="dinner-label">Obiad</span>
-          </div>
-          <div className="listing-v1-slot__zoom">
-            <div className="zoom-box">
-              <CiInstagram />
-              <span>Zobacz</span>
-            </div>
-          </div>
-        </Link>
+          </Link>
+        ))}
       </div>
     </>
   );
---
+++ src/data/latest-recipes.js
@@ -0,0 +1,46 @@
+import recipeImg001 from "./../../public/img/recipes/001_recipe.jpg";
+import recipeImg002 from "./../../public/img/recipes/002_recipe.jpg";
+import recipeImg003 from "./../../public/img/recipes/003_recipe.jpg";
+
+export const latestRecipesData = [
+    {
+      id: 1,
+      href: "https://www.instagram.com/p/DBnv7v3OYgM/",
+      imgSrc: recipeImg002,
+      altText: "Makaron",
+      label: "Śniadanie",
+      labelClass: "breakfast-label",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum quam laboriosam in tempore veritatis perspiciatis quo expedita officiis eaque esse recusandae aperiam maxime impedit, deleniti sequi similique animi voluptate nesciunt.",
+    },
+    {
+      id: 2,
+      href: "https://www.instagram.com/p/DBdvWQouPv2/",
+      imgSrc: recipeImg001,
+      altText: "Makaron",
+      label: "Obiad",
+      labelClass: "dinner-label",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum quam laboriosam in tempore veritatis perspiciatis quo expedita officiis eaque esse recusandae aperiam maxime impedit, deleniti sequi similique animi voluptate nesciunt.",
+    },
+    {
+      id: 3,
+      href: "https://www.instagram.com/p/DBrI559o2Eb/",
+      imgSrc: recipeImg003,
+      altText: "Makaron",
+      label: "Deser",
+      labelClass: "dessert-label",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum quam laboriosam in tempore veritatis perspiciatis quo expedita officiis eaque esse recusandae aperiam maxime impedit, deleniti sequi similique animi voluptate nesciunt.",
+    },
+    {
+      id: 4,
+      href: "https://www.instagram.com/p/DBdvWQouPv2/",
+      imgSrc: recipeImg001,
+      altText: "Makaron",
+      label: "Obiad",
+      labelClass: "dinner-label",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum quam laboriosam in tempore veritatis perspiciatis quo expedita officiis eaque esse recusandae aperiam maxime impedit, deleniti sequi similique animi voluptate nesciunt.",
+    },
+  ];
