1a0cc765154fe7739cce84a228b79e695d2ef1b0
goldipl <mgodlewski92@gmail.com>
2024-10-31 08:42:30 +0100

    fill celiac disease info section & add styles

--- src/components/main-page/CeliacDiseaseSection.tsx
+++ src/components/main-page/CeliacDiseaseSection.tsx
@@ -6,118 +6,141 @@ const CeliacDiseaseSection = () => {
       <div className="celiac-disease-wrapper">
         <h2>Celiakia - podstawowe informacje</h2>
 
-        <ul>
-          <li>
-            <strong>Czym jest celiakia?</strong> To przewlekła choroba
-            autoimmunologiczna, w której spożycie glutenu prowadzi do
-            uszkodzenia jelita cienkiego. Gluten to białko występujące w
-            pszenicy, życie, jęczmieniu i owsie (chyba że jest specjalnie
-            oznaczony jako bezglutenowy).
-          </li>
+        <div className="item">
+          <div className="number">1</div>
+          <strong>Czym jest celiakia?</strong>
+          <p>
+            To przewlekła choroba autoimmunologiczna, w której spożycie glutenu
+            prowadzi do uszkodzenia jelita cienkiego. Gluten to białko
+            występujące w pszenicy, życie, jęczmieniu i owsie (chyba że jest
+            specjalnie oznaczony jako bezglutenowy).
+          </p>
+        </div>
 
-          <li>
-            <strong>Przyczyny</strong> Celiakia jest częściowo uwarunkowana
-            genetycznie. Może ujawnić się na każdym etapie życia, często po
-            infekcjach, ciąży, stresie lub innych wydarzeniach wywołujących
-            stres dla organizmu.
-          </li>
+        <div className="item">
+          <div className="number">2</div>
+          <strong>Przyczyny</strong>
+          <p>
+            Celiakia jest częściowo uwarunkowana genetycznie. Może ujawnić się
+            na każdym etapie życia, często po infekcjach, ciąży, stresie lub
+            innych wydarzeniach wywołujących stres dla organizmu.
+          </p>
+        </div>
 
-          <li>
-            <strong>Objawy</strong> Objawy są zróżnicowane i mogą obejmować ból
-            brzucha, biegunki, wzdęcia, zmęczenie, utratę masy ciała, a także
-            objawy nietypowe, jak anemię, bóle stawów, problemy skórne i
-            zaburzenia neurologiczne.
-          </li>
-        </ul>
+        <div className="item">
+          <div className="number">3</div>
+          <strong>Objawy</strong>
+          <p>
+            Objawy są zróżnicowane i mogą obejmować ból brzucha, biegunki,
+            wzdęcia, zmęczenie, utratę masy ciała, a także objawy nietypowe, jak
+            anemię, bóle stawów, problemy skórne i zaburzenia neurologiczne.
+          </p>
+        </div>
 
         <h2>Co robić po diagnozie celiakii?</h2>
 
-        <ol>
-          <li>
-            <strong>Przejdź na dietę bezglutenową</strong>
-            <ul>
-              <li>
-                Kluczowe jest całkowite wyeliminowanie glutenu z diety. Nawet
-                śladowe ilości mogą powodować uszkodzenie jelit, nawet jeśli nie
-                wywołują natychmiastowych objawów.
-              </li>
-              <li>
-                Unikaj produktów zawierających pszenicę, żyto, jęczmień i zwykły
-                owies (chyba że specjalnie oznaczony jako bezglutenowy).
-              </li>
-            </ul>
-          </li>
-
-          <li>
-            <strong>Wizyta u dietetyka</strong>
+        <div className="item">
+          <div className="number">1</div>
+          <strong>Przejdź na dietę bezglutenową</strong>
+          <div className="subitem">
             <p>
-              Warto skonsultować się z dietetykiem specjalizującym się w diecie
-              bezglutenowej. Pomoże on w ułożeniu odpowiednio zbilansowanego
-              jadłospisu, dostosowanego do indywidualnych potrzeb i zapobiegnie
-              niedoborom.
+              Kluczowe jest całkowite wyeliminowanie glutenu z diety. Nawet
+              śladowe ilości mogą powodować uszkodzenie jelit, nawet jeśli nie
+              wywołują natychmiastowych objawów.
             </p>
-          </li>
-
-          <li>
-            <strong>Monitorowanie zdrowia i regularne badania</strong>
+          </div>
+          <div className="subitem">
             <p>
-              Celiakia może wpływać na wchłanianie składników odżywczych, więc
-              ważne są regularne badania, np. poziomu witamin (zwłaszcza witamin
-              D, B12, żelaza), minerałów (wapń) i densytometria (badanie
-              gęstości kości).
+              Unikaj produktów zawierających pszenicę, żyto, jęczmień i zwykły
+              owies (chyba że specjalnie oznaczony jako bezglutenowy).
             </p>
-          </li>
+          </div>
+        </div>
 
-          <li>
-            <strong>Unikaj kontaminacji glutenem</strong>
-            <ul>
-              <li>
-                W kuchni domowej zachowaj ostrożność, np. używaj osobnych
-                przyborów kuchennych, tostera czy patelni, aby zapobiec
-                przypadkowemu spożyciu glutenu.
-              </li>
-              <li>
-                Uważaj także na kosmetyki, leki i suplementy, które mogą
-                zawierać gluten.
-              </li>
-            </ul>
-          </li>
+        <div className="item">
+          <div className="number">2</div>
+          <strong>Wizyta u dietetyka</strong>
+          <p>
+            Warto skonsultować się z dietetykiem specjalizującym się w diecie
+            bezglutenowej. Pomoże on w ułożeniu odpowiednio zbilansowanego
+            jadłospisu, dostosowanego do indywidualnych potrzeb i zapobiegnie
+            niedoborom.
+          </p>
+        </div>
 
-          <li>
-            <strong>Szukanie wsparcia i edukacja</strong>
-            <ul>
-              <li>
-                Istnieje wiele grup wsparcia online i lokalnych dla osób z
-                celiakią, które oferują porady i wsparcie emocjonalne.
-              </li>
-              <li>
-                Warto też edukować się na temat składników i oznaczeń żywności,
-                by unikać produktów z ukrytym glutenem.
-              </li>
-            </ul>
-          </li>
+        <div className="item">
+          <div className="number">3</div>
+          <strong>Monitorowanie zdrowia i regularne badania</strong>
+          <p>
+            Celiakia może wpływać na wchłanianie składników odżywczych, więc
+            ważne są regularne badania, np. poziomu witamin (zwłaszcza witamin
+            D, B12, żelaza), minerałów (wapń) i densytometria (badanie gęstości
+            kości).
+          </p>
+        </div>
 
-          <li>
-            <strong>Informuj otoczenie o swojej diecie</strong>
-            <ul>
-              <li>
-                Poinformuj rodzinę, przyjaciół oraz osoby, z którymi jesz
-                posiłki (np. w pracy), o swoim stanie zdrowia, aby uniknąć
-                przypadkowych sytuacji z glutenem.
-              </li>
-              <li>
-                W restauracjach zawsze pytaj o składniki i przygotowanie dań.
-              </li>
-            </ul>
-          </li>
-        </ol>
+        <div className="item">
+          <div className="number">4</div>
+          <strong>Unikaj kontaminacji glutenem</strong>
+          <div className="subitem">
+            <p>
+              W kuchni domowej zachowaj ostrożność, np. używaj osobnych
+              przyborów kuchennych, tostera czy patelni, aby zapobiec
+              przypadkowemu spożyciu glutenu.
+            </p>
+          </div>
+          <div className="subitem">
+            <p>
+              Uważaj także na kosmetyki, leki i suplementy, które mogą zawierać
+              gluten.
+            </p>
+          </div>
+        </div>
 
-        <p>
-          Dieta bezglutenowa nie tylko eliminuje objawy, ale też chroni przed
-          długoterminowymi komplikacjami zdrowotnymi celiakii, takimi jak
-          osteoporoza, niedokrwistość czy ryzyko innych chorób
-          autoimmunologicznych.
-        </p>
+        <div className="item">
+          <div className="number">5</div>
+          <strong>Szukanie wsparcia i edukacja</strong>
+          <div className="subitem">
+            <p>
+              Istnieje wiele grup wsparcia online i lokalnych dla osób z
+              celiakią, które oferują porady i wsparcie emocjonalne.
+            </p>
+          </div>
+          <div className="subitem">
+            <p>
+              Warto też edukować się na temat składników i oznaczeń żywności, by
+              unikać produktów z ukrytym glutenem.
+            </p>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="number">6</div>
+          <strong>Informuj otoczenie o swojej diecie</strong>
+          <div className="subitem">
+            <p>
+              Poinformuj rodzinę, przyjaciół oraz osoby, z którymi jesz posiłki
+              (np. w pracy), o swoim stanie zdrowia, aby uniknąć przypadkowych
+              sytuacji z glutenem.
+            </p>
+          </div>
+          <div className="subitem">
+            <p>W restauracjach zawsze pytaj o składniki i przygotowanie dań.</p>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="number">7</div>
+          <strong>Pamiętaj</strong>
+          <div className="subitem">
+            <p>
+              Dieta bezglutenowa nie tylko eliminuje objawy, ale też chroni
+              przed długoterminowymi komplikacjami zdrowotnymi celiakii, takimi
+              jak osteoporoza, niedokrwistość czy ryzyko innych chorób
+              autoimmunologicznych.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
