979cbca7d47fa5887b6b0c7e704c74b545e54007
goldipl <mgodlewski92@gmail.com>
2024-10-31 09:00:33 +0100

    add contact section

--- src/components/main-page/Contact.tsx
+++ src/components/main-page/Contact.tsx
@@ -1,10 +1,35 @@
+import Link from "next/link";
 import React from "react";
+import { SlEnvolopeLetter } from "react-icons/sl";
+import { CiInstagram } from "react-icons/ci";
 
 const Contact = () => {
   return (
     <div id="kontakt" className="contact-section">
       <div className="contact-wrapper">
         <h2>Kontakt</h2>
+        <div className="contact-box">
+          <div className="contact-box-slot">
+            <div className="contact-box-slot__icon">
+              <SlEnvolopeLetter />
+            </div>
+            <div className="contact-box-slot__content">
+              <Link href="mailto:karolinawontorska@gmail.com">
+                karolinawontorska@gmail.com
+              </Link>
+            </div>
+          </div>
+          <div className="contact-box-slot">
+            <div className="contact-box-slot__icon instagram">
+              <CiInstagram />
+            </div>
+            <div className="contact-box-slot__content">
+              <Link href="https://www.instagram.com/bezglutenowakarola/">
+                @bezglutenowakarola
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
