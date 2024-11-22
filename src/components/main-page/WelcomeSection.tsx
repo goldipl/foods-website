6136318f9e791e0c8d87204c5175ec134016e13e
goldipl <mgodlewski92@gmail.com>
2024-11-22 15:08:46 +0100

    update content

--- src/components/main-page/WelcomeSection.tsx
+++ src/components/main-page/WelcomeSection.tsx
@@ -17,33 +17,57 @@ const WelcomeSection = () => {
             ></Image>
           </div>
           <div className="text-box">
-            <h2>Codziennie świeże informacje i pyszne przepisy</h2>
-            <h1>
-              Bezglutenowa Karola - bezglutenowe życie pełne smaków i aromatów
-            </h1>
+            <h2>Cześć! Miło mi że tu jesteś 😊</h2>
+
             <p>
-              Witaj w moim świecie! Nazywam się Karolina Wontorska, a życie bez
-              glutenu to moja codzienność – zmierzyłam się z celiakią w lipcu
-              2023 roku i od tego czasu dzielę się tutaj swoimi doświadczeniami,
-              sprawdzonymi przepisami i praktycznymi poradami.
+              Jeśli tu trafiłaś/eś, zapewne temat bezglutenowej diety nie jest
+              Ci z jakiegoś powodu obcy. Rozgość się, a ja postaram się Ci w tej
+              sytuacji pomóc.
             </p>
+
             <p>
-              Znajdziesz u mnie przepisy na potrawy, które nie tylko są
-              bezpieczne dla osób z nietolerancją glutenu, ale też smakowite i
-              proste w przygotowaniu. Bezglutenowa Karola to miejsce, gdzie mogę
-              wyrażać swoją pasję do gotowania i odkrywania nowych smaków.
+              Nazywam się <strong>Karolina Wontorska</strong>, od dziecka zmagam
+              się z chorobami autoimmunologicznymi, najpierw zdiagnozowano u
+              mnie łysienie plackowate, potem hashimoto, a w czerwcu 2023 roku
+              celiakię.
             </p>
+
             <p>
-              Polecam również restauracje specjalizujące się w kuchni
-              bezglutenowej oraz dzielę się wskazówkami, jak świadomie i
-              bezpiecznie wybierać produkty na co dzień. Dla mnie i całej
-              społeczności Bezglutenowej Karoli zdrowie i smak zawsze idą w
-              parze.
+              Czy był to dla mnie szok? Tak, ale jednocześnie ulga, że po roku
+              poszukiwań, miałam już diagnozę.
             </p>
+
             <p>
-              Zapraszam do wspólnego odkrywania, smakowania i dzielenia się
-              wyjątkowymi przepisami. Razem stwórzmy zdrową, pełną smaków
-              przyszłość, pełną kulinarnych przygód!
+              Miałam to szczęście, że przejście na bezglutenową dietę było dla
+              mnie dość prostym i płynnym procesem, bo już wcześniej, od 2020
+              gluten mocno ograniczyłam, podobnie jak cukier czy nabiał. Nie
+              jadłam takich typowych glutenowych produktów jak pizza, pierogi,
+              makarony, odkryłam inne mąki niż pszenna, unikałam pieczywa, ale w
+              innych produktach gluten był - nie miałam wtedy pełnej świadomości
+              i konieczności by wiedzieć gdzie on może się znajdować i że
+              śladowe ilości glutenu są istotne. Wtedy odżyłam, czułam się
+              wreszcie dobrze, wyglądałam dobrze. Miałam też wtedy w swoim życiu
+              pewien moment, w którym postanowiłam po prostu zdrowo się
+              odżywiać, wtedy też zaczęłam działać w kulinarnym kierunku na
+              Instagramie.
+            </p>
+
+            <p>
+              Jak to się stało, że wówczas nie zbadałam się w kierunku celiakii?
+              Nie przypuszczałam że mogę to mieć i zwyczajnie nie wiedziałam na
+              temat tej choroby zbyt wiele.
+            </p>
+
+            <p>
+              Natomiast gdy w 2022 zaczęłam bardzo chudnąć, czuć się ciągle
+              zmęczona, wyniszczona, moje włosy wypadały z dnia na dzień, poziom
+              ferrytyny był na bardzo niskim poziomie i pojawiały się dodatkowe
+              dziwne objawy jak np krwawienie z odbytu, badania były konieczne.
+              Trafiałam na różnych lekarzy i pomimo ich wymownych sugestii, że
+              „sama szukam sobie choroby” byłam zmotywowana, aż trafiłam na
+              kogoś kto połączył wszystkie kropki. Niedługo potem byłam po
+              gastroskopii, kolonoskopii, a potem była diagnoza:{" "}
+              <strong>CELIAKIA</strong>.
             </p>
           </div>
         </div>
