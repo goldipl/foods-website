9f1a65d7f132b494a96d1b49af7fbc568f6225a8
goldipl <mgodlewski92@gmail.com>
2024-11-23 21:43:49 +0100

    update desserts

--- src/data/recipes/desserts-recipes.js
+++ src/data/recipes/desserts-recipes.js
@@ -1,11 +1,21 @@
 import rozkiAlaOreo from "./../../../public/img/recipes/desserts/001_bezglutenowe_rozki_a_la_oreo.jpg";
 import cynamonki from "./../../../public/img/recipes/desserts/002_cynamonki.jpg";
-import czekoladoweCynamonki from "./../../../public/img/recipes/desserts/003_czekoladowe_cynamonki.jpg";
+import kokosoweTiramisu from "./../../../public/img/recipes/desserts/003_kokosowe_tiramisu.jpg";
 import wegeSernik from "./../../../public/img/recipes/desserts/004_wege_sernik.jpg";
 
 export const dessertsRecipesData = [
     {
       id: 1,
+      href: "https://www.instagram.com/p/C_XerbBoQV-/",
+      imgSrc: kokosoweTiramisu,
+      altText: "Kokosowe tiramisu - bez głutenu i bez nabiału",
+      label: "Deser",
+      labelClass: "dessert-label",
+      description:
+        "Kokosowe tiramisu - bez głutenu i bez nabiału",
+    },
+    {
+      id: 2,
       href: "https://www.instagram.com/p/DBrI559o2Eb/",
       imgSrc: rozkiAlaOreo,
       altText: "Bezglutenowe rożki z nadzieniem a'la Oreo",
@@ -15,7 +25,7 @@ export const dessertsRecipesData = [
         "Bezglutenowe rożki z nadzieniem a'la Oreo",
     },
     {
-      id: 2,
+      id: 3,
       href: "https://www.instagram.com/reel/DAgcKLeITeH/",
       imgSrc: cynamonki,
       altText: "Bezglutenowe cynamonki - musisz je spróbwać",
@@ -23,16 +33,6 @@ export const dessertsRecipesData = [
       labelClass: "dessert-label",
       description:
         "Bezglutenowe cynamonki - musisz je spróbwać",
-    },
-    {
-      id: 3,
-      href: "https://www.instagram.com/reel/DB6Zle4oftX/",
-      imgSrc: czekoladoweCynamonki,
-      altText: "Czekoladowe bezglutenowe cynamonki z Nutellą",
-      label: "Deser",
-      labelClass: "dessert-label",
-      description:
-        "Czekoladowe bezglutenowe cynamonki z Nutellą",
     },
     {
       id: 4,
