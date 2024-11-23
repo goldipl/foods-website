d8e3dd416c2a4b1f37cd9e920f71689720bc5ec2
goldipl <mgodlewski92@gmail.com>
2024-11-23 22:38:54 +0100

    update & add sample footer columns links

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -7,6 +7,28 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-wrapper">
+        <div className="footer-wrapper-top">
+          <div className="footer-col">
+            <h2>Przepisy</h2>
+            <Link href="#sniadania">Śniadania</Link>
+            <Link href="#obiady">Obiady</Link>
+            <Link href="#desery">Desery</Link>
+          </div>
+          <div className="footer-col">
+            <h2>Przepisy</h2>
+            <Link href="#sniadania">Śniadania</Link>
+            <Link href="#obiady">Obiady</Link>
+            <Link href="#desery">Desery</Link>
+          </div>
+          <div className="footer-col">
+            <h2>Przepisy</h2>
+            <Link href="#sniadania">Śniadania</Link>
+            <Link href="#obiady">Obiady</Link>
+            <Link href="#desery">Desery</Link>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
         <p>@BezglutenowaKarola © {currentYear}</p>
         <span>
           Projekt i realizacja:
