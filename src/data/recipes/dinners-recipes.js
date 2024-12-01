f6a98f40f9c965e6061f4e64c3ada03ad0d63d1d
goldipl <mgodlewski92@gmail.com>
2024-12-01 11:48:25 +0100

    add a new dinner - łazanki

--- src/data/recipes/dinners-recipes.js
+++ src/data/recipes/dinners-recipes.js
@@ -2,6 +2,7 @@ import dyniowyMakaronzSzynkaSpeck from "./../../../public/img/recipes/dinners/00
 import makaronzWeganskimBialymSerem from "./../../../public/img/recipes/dinners/002_makaron_z_weganskim_bialym_serem.jpg";
 import pizzazRukola from "./../../../public/img/recipes/dinners/003_bezglutenowa_pizza_z_rukola.jpg";
 import BurgerTeriyakizKurczaka from "./../../../public/img/recipes/dinners/004_bezglutenowy_burger_z_kurczaka_teriyaki.png";
+import bezglutenoweLazanki from "./../../../public/img/recipes/dinners/005_bezglutenowe_lazanki.jpg";
 
 
 export const dinnersRecipesData = [
@@ -44,5 +45,15 @@ export const dinnersRecipesData = [
       labelClass: "dinner-label",
       description:
         "Bezglutenowy kurczako-burger w stylu Teriyaki",
+    },
+    {
+      id: 5,
+      href: "https://www.instagram.com/p/DC_zuLFodeV/",
+      imgSrc: bezglutenoweLazanki,
+      altText: "Bezglutenowe łazanki - idealny przepis na świąteczny stół",
+      label: "Obiad",
+      labelClass: "dinner-label",
+      description:
+        "Bezglutenowe łazanki - idealny przepis na świąteczny stół",
     },
   ];
