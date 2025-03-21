7853f5db6b54a1ad4e3c909f7f4fbd65fa12e069
goldipl <mgodlewski92@gmail.com>
2025-03-21 16:11:06 +0100

    update Topbar component with new discount code and adjust styles

--- src/components/common/Topbar.tsx
+++ src/components/common/Topbar.tsx
@@ -7,10 +7,23 @@ const Topbar = () => {
     <div id="topbar" className="topbar">
       <div className="topbar-wrapper">
         <p>
-          Kod <strong>10%</strong> zniżki <code>Karola10GTX</code> na produkty w
-          sklepie
+          Kod <strong>10%</strong> zniżki <code>Karola10GTX</code> w sklepie
           <Link target="_blank" href="https://sklep.glutenex.com.pl/">
             Glutenex
+            <Image
+              className="cart-icon"
+              src={cartIcon}
+              width={24}
+              height={24}
+              alt="koszyk"
+            ></Image>
+          </Link>
+        </p>
+        <p>
+          Kod <strong>10%</strong> zniżki <code>bezglutenowakarola10</code> w
+          sklepie
+          <Link target="_blank" href="https://www.sunday-natural.pl/">
+            Sunday Natural
             <Image
               className="cart-icon"
               src={cartIcon}
