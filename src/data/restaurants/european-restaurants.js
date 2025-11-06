eeb3947b7e9b086c8dac3f43342ccc0706261fad
goldipl <mgodlewski92@gmail.com>
2025-11-06 10:13:51 +0100

    add new restaurants

--- src/data/restaurants/european-restaurants.js
+++ src/data/restaurants/european-restaurants.js
@@ -1,43 +1,108 @@
-import zenZeroLidoDiOstia from "./../../../public/img/restaurants/europe/001_zenzero_lido_di_ostia.jpg";
-import mamaEatRoma from "./../../../public/img/restaurants/europe/002_mama_eat_rzym_wlochy.jpg";
-import laSoffittaRoma from "./../../../public/img/restaurants/europe/003_la_soffitta_rzym_wlochy.jpg";
-import burgerShopCypr from "./../../../public/img/restaurants/europe/004_burger_shop_nikozja_cypr.jpg";
-import bezglutenowaRumunia from "./../../../public/img/restaurants/europe/005_bezglutenowa_rumunia.jpg";
-
+import burgerShopCypr from "./../../../public/img/restaurants/europe/010_burger_shop_nikozja_cypr.jpg";
+import bezglutenowaRumunia from "./../../../public/img/restaurants/europe/011_bezglutenowa_rumunia.jpg";
+import lidoDiOstia1 from "./../../../public/img/restaurants/europe/001_lido_di_ostia_cz1.jpg";
+import lidoDiOstia2 from "./../../../public/img/restaurants/europe/002_lido_di_ostia_cz2.jpg";
+import lidoDiOstia3 from "./../../../public/img/restaurants/europe/003_lido_di_ostia_cz3.jpg";
+import rzymCz1 from "./../../../public/img/restaurants/europe/004_rzym_cz1.jpg";
+import rzymCz2 from "./../../../public/img/restaurants/europe/005_rzym_cz2.jpg";
+import rzymCz3 from "./../../../public/img/restaurants/europe/006_rzym_cz3.jpg";
+import rzymCz4 from "./../../../public/img/restaurants/europe/007_rzym_cz4.jpg";
+import rzymCz5 from "./../../../public/img/restaurants/europe/008_rzym_cz5.jpg";
+import florencjaCz1 from "./../../../public/img/restaurants/europe/009_florencja_cz1.jpg";
 
 export const  europeanRestaurantsData = [
     {
       id: 1,
-      href: "https://www.instagram.com/p/DBHExqroFiQ/",
-      imgSrc: zenZeroLidoDiOstia,
-      altText: "Restauracja ZenZero, Lido di Ostia, Rzym, Włochy",
+      href: "https://www.instagram.com/p/DABZsxyId-Q/",
+      imgSrc: lidoDiOstia1,
+      altText: "Lido di Ostia cz. 1, Rzym, Włochy",
       label: "Włochy",
       labelClass: "europe-label",
       description:
-        "Restauracja ZenZero, Lido di Ostia, Rzym, Włochy",
+        "Lido di Ostia cz. 1, Rzym, Włochy",
     },
     {
       id: 2,
-      href: "https://www.instagram.com/p/DAMP3HvIRvd/",
-      imgSrc: mamaEatRoma,
-      altText: "Restauracja Mama Eat, Rzym, Włochy",
+      href: "https://www.instagram.com/p/DATr2B4IgDf/",
+      imgSrc: lidoDiOstia2,
+      altText: "Lido di Ostia cz. 2, Rzym, Włochy",
       label: "Włochy",
       labelClass: "europe-label",
       description:
-        "Restauracja Mama Eat, Rzym, Włochy",
+        "Lido di Ostia cz. 2, Rzym, Włochy",
     },
     {
       id: 3,
-      href: "https://www.instagram.com/p/DBVkMzgI27-/",
-      imgSrc: laSoffittaRoma,
-      altText: "Restauracja La Soffitta, Rzym, Włochy",
+      href: "https://www.instagram.com/p/DBHExqroFiQ/",
+      imgSrc: lidoDiOstia3,
+      altText: "Lido di Ostia cz. 3, Rzym, Włochy",
       label: "Włochy",
       labelClass: "europe-label",
       description:
-        "Restauracja La Soffitta, Rzym, Włochy",
+        "Lido di Ostia cz. 3, Rzym, Włochy",
     },
     {
       id: 4,
+      href: "https://www.instagram.com/p/DAKxCTaIF9Y/",
+      imgSrc: rzymCz1,
+      altText: "Rzym cz. 1, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Rzym cz. 1, Włochy",
+    },
+    {
+      id: 5,
+      href: "https://www.instagram.com/p/DANYqGLIWiO/",
+      imgSrc: rzymCz2,
+      altText: "Rzym cz. 2, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Rzym cz. 2, Włochy",
+    },
+    {
+      id: 6,
+      href: "https://www.instagram.com/p/DAcz8_QIabG/",
+      imgSrc: rzymCz3,
+      altText: "Rzym cz. 3, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Rzym cz. 3, Włochy",
+    },
+    {
+      id: 7,
+      href: "https://www.instagram.com/p/DAxk19ko_cj/",
+      imgSrc: rzymCz4,
+      altText: "Rzym cz. 4, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Rzym cz. 4, Włochy",
+    },
+    {
+      id: 8,
+      href: "https://www.instagram.com/p/DBVkMzgI27-/",
+      imgSrc: rzymCz5,
+      altText: "Watykan, Rzym cz. 5, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Watykan, Rzym cz. 5, Włochy",
+    },
+    {
+      id: 9,
+      href: "https://www.instagram.com/p/DAkgyrjI0VP/",
+      imgSrc: florencjaCz1,
+      altText: "Florencja, Włochy",
+      label: "Włochy",
+      labelClass: "europe-label",
+      description:
+        "Florencja, Włochy",
+    },
+    {
+      id: 10,
       href: "https://www.instagram.com/p/C3IhmmhI9JO/",
       imgSrc: burgerShopCypr,
       altText: "Burger Shop, Nikozja, Cypr",
@@ -47,7 +112,7 @@ export const  europeanRestaurantsData = [
         "Burger Shop, Nikozja, Cypr",
     },
     {
-      id: 5,
+      id: 11,
       href: "https://www.instagram.com/p/DN8eqvgCKd3/",
       imgSrc: bezglutenowaRumunia,
       altText: "10 bezglutenowych miejsc/restauracji w Rumunii",
