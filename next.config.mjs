fee7cffe8686dba0f054dffc7a23a5ce93c93260
goldipl <mgodlewski92@gmail.com>
2025-11-07 13:32:16 +0100

    rollback redirect url changes

--- next.config.mjs
+++ next.config.mjs
@@ -6,14 +6,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/sniadania/',
-        destination: '/breakfasts-page/',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
