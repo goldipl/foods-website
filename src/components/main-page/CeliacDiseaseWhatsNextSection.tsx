2c448d161c929a4ceac43c145c3ddd1b1477b56f
goldipl <mgodlewski92@gmail.com>
2025-11-09 09:16:10 +0100

    reorganise celiac disease section and separate to two different sections

---
+++ src/components/main-page/CeliacDiseaseWhatsNextSection.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+
+const CeliacDiseaseWhatsNextSection = () => {
+  return (
+    <section className="celiac-disease-section info-title">
+      <div className="celiac-disease-wrapper">
+        <div className="section-title">
+          <h2 id="pierwsze-kroki-po-diagnozie">
+            Celiakia - i co dalej? <br></br>Pierwsze kroki po diagnozie
+          </h2>
+        </div>
+
+        <div className="item">
+          <div className="number">1</div>
+          <strong>W kuchni</strong>
+          <p>Wymień wszystkie przyprawy w kuchni na bezglutenowe</p>
+        </div>
+
+        <div className="item">
+          <div className="number">2</div>
+          <strong>Zakupy</strong>
+          <p>
+            Produkty sypkie jak mąki, kasze, ryże kupuj tylko ze znaczkiem
+            przekreślonego kłosa
+          </p>
+        </div>
+
+        <div className="item">
+          <div className="number">3</div>
+          <strong>Czytaj etykiety</strong>
+          <p>
+            Nie panikuj! Wiele produktów w każdym sklepie jest bezglutenowa -
+            tylko na pierwszy rzut oka tego nie widać - musisz nauczyć się
+            czytać składy
+          </p>
+        </div>
+
+        <div className="item">
+          <div className="number">4</div>
+          <strong>Nie kupuj gotowych mięs na grilla</strong>
+          <p>
+            Unikaj gotowych mięs na grilla lub innych mieszanek, lepiej wybierz
+            czyste mięso i dopraw w domu
+          </p>
+        </div>
+
+        <div className="item">
+          <div className="number">5</div>
+          <strong>Porządek w kuchni</strong>
+          <p>
+            Jeśli dzielisz kuchnię z osobami jedzącymi gluten, dbaj o czystość i
+            osobne przybory kuchenne
+          </p>
+        </div>
+
+        <div className="item">
+          <div className="number">6</div>
+          <strong>Przechowywanie produktów</strong>
+          <p>Nie trzymaj razem produktów glutenowych z bezglutenowymi</p>
+        </div>
+
+        <div className="item">
+          <div className="number">7</div>
+          <strong>Opakowania</strong>
+          <p>Kup sobie praktyczne pudełka i torbę termiczną</p>
+        </div>
+
+        <div className="item">
+          <div className="number">8</div>
+          <strong>Wizyta u dietetyka</strong>
+          <p>
+            Umów się na choć jedną konsultację z dietetykiem, aby omówić
+            szczegóły swojej nowej diety
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CeliacDiseaseWhatsNextSection;
