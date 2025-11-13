924a69fc6a69a6133b62cd4b0ad9b02f77b5e92e
goldipl <mgodlewski92@gmail.com>
2025-11-13 17:57:11 +0100

    add new map markers and restyling popup description

--- src/components/map/MapComponent.tsx
+++ src/components/map/MapComponent.tsx
@@ -54,13 +54,15 @@ const MapComponent: React.FC<MapComponentProps> = ({
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
             url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           />
-          <Marker position={[52.230471, 21.013947]} icon={greenIcon}>
+
+          <Marker key={1} position={[52.230625, 21.014178]} icon={greenIcon}>
             <Popup>
-              <div>Rola Pao Restaurant</div>
+              <strong>Rola Pao</strong>
               <div>Al. Jerozolimskie 42,</div>
               <div>00-042 Warszawa</div>
               <div>
                 <Link
+                  className="map-link"
                   target="_blank"
                   href="https://www.instagram.com/reel/C-S0V-rIpJ0/"
                 >
@@ -69,15 +71,51 @@ const MapComponent: React.FC<MapComponentProps> = ({
               </div>
             </Popup>
           </Marker>
-          <Marker position={[52.233997, 21.001116]} icon={greenIcon}>
+
+          <Marker key={2} position={[52.233997, 21.001116]} icon={greenIcon}>
             <Popup>
-              <div>Patila Döner Kebap</div>
+              <strong>Patila Döner Kebap</strong>
               <div>ul. Świętokrzyska 34,</div>
               <div>00-116 Warszawa</div>
               <div>
                 <Link
+                  className="map-link"
                   target="_blank"
                   href="https://www.instagram.com/p/DOinsrFiKNw/"
+                >
+                  Zobacz to miejsce
+                </Link>
+              </div>
+            </Popup>
+          </Marker>
+
+          <Marker key={3} position={[52.221044, 21.016888]} icon={greenIcon}>
+            <Popup>
+              <strong>Restauracja Wiesz Co Zjesz</strong>
+              <div>plac Konstytucji 1,</div>
+              <div>00-647 Warszawa</div>
+              <div>
+                <Link
+                  className="map-link"
+                  target="_blank"
+                  href="https://www.instagram.com/p/DGaKXYkoBiV/"
+                >
+                  Zobacz to miejsce
+                </Link>
+              </div>
+            </Popup>
+          </Marker>
+
+          <Marker key={4} position={[52.221629, 21.015114]} icon={greenIcon}>
+            <Popup>
+              <strong>Groole</strong>
+              <div>Jana i Jędrzeja Śniadeckich 8,</div>
+              <div>00-656 Warszawa</div>
+              <div>
+                <Link
+                  className="map-link"
+                  target="_blank"
+                  href="https://www.instagram.com/p/DQKEYzmCNZg/"
                 >
                   Zobacz to miejsce
                 </Link>
