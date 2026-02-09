18b83f0814de27c89ac1e4ca3737b1b9a74805df
goldipl <mgodlewski92@gmail.com>
2026-02-09 09:48:53 +0100

    refactor and divide code for PARTNERS consts

--- src/components/main-page/CooperationSection.tsx
+++ src/components/main-page/CooperationSection.tsx
@@ -3,26 +3,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { PARTNERS } from "@/utils/consts";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-import Glutenex from "@/public/img/cooperation/glutenex.png";
-import Incola from "@/public/img/cooperation/incola.jpg";
-import Bezgluten from "@/public/img/cooperation/bezgluten.png";
-import Balviten from "@/public/img/cooperation/balviten.jpg";
-import Vallongo from "@/public/img/cooperation/vallongo.jpg";
-import Moncana from "@/public/img/cooperation/moncana.png";
-
-const PARTNERS = [
-  { name: "Glutenex", href: "https://glutenex.pl/", src: Glutenex },
-  { name: "Bezgluten", href: "https://bezgluten.pl/", src: Bezgluten },
-  { name: "Incola", href: "https://www.incola.com.pl/", src: Incola },
-  { name: "Balviten", href: "https://balviten.com/", src: Balviten },
-  { name: "Vallongo", href: "https://vallongo.it/pl-pl", src: Vallongo },
-  { name: "Moncana", href: "https://moncana.pl/", src: Moncana },
-];
 
 const CooperationSection = () => {
   return (
---
+++ src/utils/consts.tsx
@@ -0,0 +1,15 @@
+import Glutenex from "@/public/img/cooperation/glutenex.png";
+import Incola from "@/public/img/cooperation/incola.jpg";
+import Bezgluten from "@/public/img/cooperation/bezgluten.png";
+import Balviten from "@/public/img/cooperation/balviten.jpg";
+import Vallongo from "@/public/img/cooperation/vallongo.jpg";
+import Moncana from "@/public/img/cooperation/moncana.png";
+
+export const PARTNERS = [
+  { name: "Glutenex", href: "https://glutenex.pl/", src: Glutenex },
+  { name: "Bezgluten", href: "https://bezgluten.pl/", src: Bezgluten },
+  { name: "Incola", href: "https://www.incola.com.pl/", src: Incola },
+  { name: "Balviten", href: "https://balviten.com/", src: Balviten },
+  { name: "Vallongo", href: "https://vallongo.it/pl-pl", src: Vallongo },
+  { name: "Moncana", href: "https://moncana.pl/", src: Moncana },
+];
