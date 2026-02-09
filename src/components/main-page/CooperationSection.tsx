03785fb0f3fcb40822f4165cdf19377df7b73ec3
goldipl <mgodlewski92@gmail.com>
2026-02-09 09:31:39 +0100

    refactor cooperation section

--- src/components/main-page/CooperationSection.tsx
+++ src/components/main-page/CooperationSection.tsx
@@ -1,29 +1,40 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore from "swiper";
-import { Pagination, Navigation } from "swiper/modules";
-import { Autoplay } from "swiper/modules";
 import Image from "next/image";
 import Link from "next/link";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
-import Glutenex from "./../../../public/img/cooperation/glutenex.png";
-import Incola from "./../../../public/img/cooperation/incola.jpg";
-import Bezgluten from "./../../../public/img/cooperation/bezgluten.png";
-import Balviten from "./../../../public/img/cooperation/balviten.jpg";
-import Vallongo from "./../../../public/img/cooperation/vallongo.jpg";
-import Moncana from "./../../../public/img/cooperation/moncana.png";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
-SwiperCore.use([Pagination, Navigation]);
+import Glutenex from "@/public/img/cooperation/glutenex.png";
+import Incola from "@/public/img/cooperation/incola.jpg";
+import Bezgluten from "@/public/img/cooperation/bezgluten.png";
+import Balviten from "@/public/img/cooperation/balviten.jpg";
+import Vallongo from "@/public/img/cooperation/vallongo.jpg";
+import Moncana from "@/public/img/cooperation/moncana.png";
+
+const PARTNERS = [
+  { name: "Glutenex", href: "https://glutenex.pl/", src: Glutenex },
+  { name: "Bezgluten", href: "https://bezgluten.pl/", src: Bezgluten },
+  { name: "Incola", href: "https://www.incola.com.pl/", src: Incola },
+  { name: "Balviten", href: "https://balviten.com/", src: Balviten },
+  { name: "Vallongo", href: "https://vallongo.it/pl-pl", src: Vallongo },
+  { name: "Moncana", href: "https://moncana.pl/", src: Moncana },
+];
 
 const CooperationSection = () => {
   return (
-    <section id="wspolpraca" className="cooperation-section cooperation-title">
+    <section id="wspolpraca" className="cooperation-section">
       <div className="cooperation-wrapper">
-        <div className="section-title">
+        <header className="section-title">
           <h2>Współpraca</h2>
-        </div>
-        <div className="swiper-wrapper">
+        </header>
+
+        <div className="swiper-container">
           <Swiper
+            modules={[Autoplay, Pagination, Navigation]}
             spaceBetween={30}
             slidesPerView={1}
             navigation
@@ -33,104 +44,31 @@ const CooperationSection = () => {
               delay: 2200,
               disableOnInteraction: false,
             }}
-            modules={[Autoplay, Pagination, Navigation]}
+            breakpoints={{
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
           >
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://glutenex.pl/"
-                  target="_blank"
-                >
-                  <Image
-                    src={Glutenex}
-                    alt="Glutenex"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://bezgluten.pl/"
-                  target="_blank"
-                >
-                  <Image
-                    src={Bezgluten}
-                    alt="Bezgluten"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://www.incola.com.pl/"
-                  target="_blank"
-                >
-                  <Image
-                    src={Incola}
-                    alt="Incola"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://balviten.com/"
-                  target="_blank"
-                >
-                  <Image
-                    src={Balviten}
-                    alt="Balviten"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://vallongo.it/pl-pl"
-                  target="_blank"
-                >
-                  <Image
-                    src={Vallongo}
-                    alt="Vallongo"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide>
-              <div className="slide-content">
-                <Link
-                  className="cooperation-link"
-                  href="https://moncana.pl/"
-                  target="_blank"
-                >
-                  <Image
-                    src={Moncana}
-                    alt="Moncana"
-                    width={260}
-                    height={122}
-                  ></Image>
-                </Link>
-              </div>
-            </SwiperSlide>
+            {PARTNERS.map((partner) => (
+              <SwiperSlide key={partner.name}>
+                <div className="slide-content">
+                  <Link
+                    href={partner.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="cooperation-link"
+                  >
+                    <Image
+                      src={partner.src}
+                      alt={`${partner.name} logo`}
+                      width={260}
+                      height={122}
+                      style={{ objectFit: "contain" }}
+                    />
+                  </Link>
+                </div>
+              </SwiperSlide>
+            ))}
           </Swiper>
         </div>
       </div>
