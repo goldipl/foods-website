5186b9545d74b2dbd1b52ad5981ff4ab44d2d70c
goldipl <mgodlewski92@gmail.com>
2026-02-09 11:34:47 +0100

    fix: add spacing for map section title

--- src/components/main-page/MapSection.tsx
+++ src/components/main-page/MapSection.tsx
@@ -9,7 +9,7 @@ const MapSection = () => {
     <section id="mapa" className="map-section">
       <div className="map-section-wrapper">
         <div className="section-title">
-          <h2>Mapa restauracji/miejsc gluten free</h2>
+          <h2>Mapa restauracji / miejsc gluten free</h2>
           <h4>Kliknij w mapę i sprawdź więcej miejsc.</h4>
         </div>
         <div>
@@ -20,7 +20,7 @@ const MapSection = () => {
             <Image
               className="map-section-wrapper__img"
               src={glutenFreeMap}
-              alt="Mapa restauracji/miejsc gluten free"
+              alt="Mapa restauracji / miejsc gluten free"
               width={856}
               height={506}
             />
