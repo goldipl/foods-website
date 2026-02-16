84b21aa0edac7996ac01ae910793c66f96e3092b
goldipl <mgodlewski92@gmail.com>
2026-02-16 21:24:57 +0100

    refactor: migrate Hero component to use dynamic slide data and improve structure

--- src/components/main-page/Hero.tsx
+++ src/components/main-page/Hero.tsx
@@ -1,122 +1,17 @@
 import React from "react";
 import Link from "next/link";
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import { HiChevronRight } from "react-icons/hi2";
 
+// Import data and types
+import { HERO_SLIDES, HeroSlideItem, SlideLink } from "./../../utils/consts";
+
+// Styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
-import welcomeImg from "./../../../public/img/welcome/karola-dynia.jpg";
-import breakfastImg from "./../../../public/img/recipes/breakfasts/003_sniadaniowa_tortilla_z_jajkiem.jpg";
-import dinnerImg from "./../../../public/img/recipes/dinners/004_bezglutenowy_burger_z_kurczaka_teriyaki.jpg";
-import dessertImg from "./../../../public/img/recipes/desserts/004_wege_sernik.jpg";
-import pierogi from "./../../../public/img/products/022_bezglutenowe_panettone.jpg";
-import pastaGlutenFreeRoma from "./../../../public/img/welcome/wloski-makaron-gluten-free.jpg";
-import saskaSushiWarszawa from "./../../../public/img/restaurants/poland/001_saska_sushi.jpg";
-import bezglutenoweHalloween from "./../../../public/img/events/011_bezglutenowe_halloween.jpg";
-import glutenFreeHeroMap from "./../../../public/img/map/gluten-free-hero-map.jpg";
-
-interface SlideLink {
-  label: string;
-  href: string;
-  className?: string;
-}
-
-interface SlideItem {
-  title: string;
-  subtitle: string;
-  image: StaticImageData;
-  alt: string;
-  links: SlideLink[];
-  reverse?: boolean;
-}
-
-const SLIDES_DATA: SlideItem[] = [
-  {
-    title: "Zdrowo, smacznie, bezglutenowo!",
-    subtitle: "Przepisy, restauracje i porady dla osób na diecie bezglutenowej",
-    image: welcomeImg,
-    alt: "Restauracja Rola Pao w Warszawie",
-    links: [
-      { label: "Przepisy", href: "#sniadania" },
-      {
-        label: "Poradniki",
-        href: "#celiakia-podstawowe-informacje",
-        className: "white",
-      },
-    ],
-  },
-  {
-    title: "Mapa miejsc bezglutenowych",
-    subtitle:
-      "Restauracje, kawiarnie, cukiernie bez glutenu w Polsce i na świecie sprawdzone osobiście przeze mnie",
-    image: glutenFreeHeroMap,
-    alt: "Mapa miejsc bezglutenowych",
-    links: [{ label: "Sprawdź", href: "/bezglutenowe-miejsca" }],
-  },
-  {
-    title: "Bezglutenowe śniadania",
-    subtitle:
-      "Szukasz pomysłów na śniadania bezglutenowe, które są nie tylko zdrowe, ale i pyszne?",
-    image: breakfastImg,
-    alt: "Bezglutenowa śniadaniowa tortilla z komosy ryżowej",
-    reverse: true,
-    links: [{ label: "Sprawdź przepisy", href: "#sniadania" }],
-  },
-  {
-    title: "Bezglutenowe obiady",
-    subtitle:
-      "Szukasz inspiracji na obiady bezglutenowe, które będą nie tylko zdrowe, ale też pyszne?",
-    image: dinnerImg,
-    alt: "Kurczako-burger Teriyaki",
-    links: [{ label: "Sprawdź przepisy", href: "#obiady" }],
-  },
-  {
-    title: "Bezglutenowe desery",
-    subtitle:
-      "Marzysz o pysznych deserach bezglutenowych, które zachwycą smakiem?",
-    image: dessertImg,
-    alt: "Wege sernik",
-    reverse: true,
-    links: [{ label: "Sprawdź przepisy", href: "#desery" }],
-  },
-  {
-    title: "Bezglutenowe restauracje w Polsce",
-    subtitle:
-      "Szukasz miejsc, gdzie możesz zjeść pysznie i bez glutenu w Polsce?",
-    image: saskaSushiWarszawa,
-    alt: "Saska Sushi",
-    links: [{ label: "Sprawdź", href: "#restauracje-polska" }],
-  },
-  {
-    title: "Bezglutenowe restauracje na świecie",
-    subtitle:
-      "Szukasz miejsc, gdzie możesz zjeść pysznie i bez glutenu na świecie?",
-    image: pastaGlutenFreeRoma,
-    alt: "Restauracja Mama Eat, Włochy",
-    reverse: true,
-    links: [{ label: "Sprawdź", href: "#restauracje-europa" }],
-  },
-  {
-    title: 'Bezglutenowe produkty - "Czy to ma gluten?"',
-    subtitle:
-      "Sprawdzone produkty bezglutenowe, które warto mieć w swojej kuchni",
-    image: pierogi,
-    alt: "Czy to ma gluten?",
-    links: [{ label: "Sprawdź", href: "#produkty" }],
-  },
-  {
-    title: "Bezglutenowe wydarzenia",
-    subtitle:
-      "Bezglutenowe eventy, warsztaty i spotkania dla osób na diecie bezglutenowej",
-    image: bezglutenoweHalloween,
-    alt: "Bezglutenowe wydarzenia",
-    links: [{ label: "Sprawdź", href: "#wydarzenia" }],
-  },
-];
 
 const SlideText: React.FC<{
   title: string;
@@ -140,16 +35,6 @@ const SlideText: React.FC<{
   </div>
 );
 
-const SlideImage: React.FC<{
-  src: StaticImageData;
-  alt: string;
-  priority?: boolean;
-}> = ({ src, alt, priority }) => (
-  <div className="swiper-slide-box__img">
-    <Image src={src} alt={alt} width={450} height={450} priority={priority} />
-  </div>
-);
-
 const Hero: React.FC = () => {
   return (
     <section id="hero" className="hero">
@@ -159,32 +44,40 @@ const Hero: React.FC = () => {
           slidesPerView={1}
           navigation
           pagination={{ clickable: true }}
-          loop={true}
-          autoplay={{
-            delay: 5500,
-            disableOnInteraction: false,
-          }}
+          loop
+          autoplay={{ delay: 5500, disableOnInteraction: false }}
           modules={[Autoplay, Pagination, Navigation]}
         >
-          {SLIDES_DATA.map((slide, index) => (
+          {HERO_SLIDES.map((slide, index) => (
             <SwiperSlide key={`hero-slide-${index}`}>
               <div className="slide-content">
                 <div
                   className={`swiper-slide-box ${slide.reverse ? "text-right-side" : ""}`}
                 >
-                  {/* Clean logic: Reverse slides swap the DOM order of Text/Image */}
+                  {/* If not reverse: Text then Image. If reverse: Image then Text */}
                   {!slide.reverse ? (
                     <>
                       <SlideText {...slide} />
-                      <SlideImage
-                        src={slide.image}
-                        alt={slide.alt}
-                        priority={index === 0}
-                      />
+                      <div className="swiper-slide-box__img">
+                        <Image
+                          src={slide.image}
+                          alt={slide.alt}
+                          width={450}
+                          height={450}
+                          priority={index === 0}
+                        />
+                      </div>
                     </>
                   ) : (
                     <>
-                      <SlideImage src={slide.image} alt={slide.alt} />
+                      <div className="swiper-slide-box__img">
+                        <Image
+                          src={slide.image}
+                          alt={slide.alt}
+                          width={450}
+                          height={450}
+                        />
+                      </div>
                       <SlideText {...slide} />
                     </>
                   )}
--- src/utils/consts.tsx
+++ src/utils/consts.tsx
@@ -1,3 +1,17 @@
+import { StaticImageData } from "next/image";
+
+// --- HERO SLIDE IMPORTS ---
+import welcomeImg from "../../public/img/welcome/karola-dynia.jpg";
+import breakfastImg from "../../public/img/recipes/breakfasts/003_sniadaniowa_tortilla_z_jajkiem.jpg";
+import dinnerImg from "../../public/img/recipes/dinners/004_bezglutenowy_burger_z_kurczaka_teriyaki.jpg";
+import dessertImg from "../../public/img/recipes/desserts/004_wege_sernik.jpg";
+import pierogi from "../../public/img/products/022_bezglutenowe_panettone.jpg";
+import pastaGlutenFreeRoma from "../../public/img/welcome/wloski-makaron-gluten-free.jpg";
+import saskaSushiWarszawa from "../../public/img/restaurants/poland/001_saska_sushi.jpg";
+import bezglutenoweHalloween from "../../public/img/events/011_bezglutenowe_halloween.jpg";
+import glutenFreeHeroMap from "../../public/img/map/gluten-free-hero-map.jpg";
+
+// --- PARTNER IMPORTS ---
 import Glutenex from "../../public/img/cooperation/glutenex.png";
 import Incola from "../../public/img/cooperation/incola.jpg";
 import Bezgluten from "../../public/img/cooperation/bezgluten.png";
@@ -5,11 +19,118 @@ import Balviten from "../../public/img/cooperation/balviten.jpg";
 import Vallongo from "../../public/img/cooperation/vallongo.jpg";
 import Moncana from "../../public/img/cooperation/moncana.png";
 
-export const PARTNERS = [
+// --- TYPES & INTERFACES ---
+export interface SlideLink {
+  label: string;
+  href: string;
+  className?: string;
+}
+
+export interface HeroSlideItem {
+  title: string;
+  subtitle: string;
+  image: StaticImageData;
+  alt: string;
+  links: SlideLink[];
+  reverse?: boolean;
+}
+
+export interface PartnerItem {
+  name: string;
+  href: string;
+  src: StaticImageData;
+}
+
+// --- DATA ARRAYS ---
+export const PARTNERS: PartnerItem[] = [
   { name: "Glutenex", href: "https://glutenex.pl/", src: Glutenex },
   { name: "Bezgluten", href: "https://bezgluten.pl/", src: Bezgluten },
   { name: "Incola", href: "https://www.incola.com.pl/", src: Incola },
   { name: "Balviten", href: "https://balviten.com/", src: Balviten },
   { name: "Vallongo", href: "https://vallongo.it/pl-pl", src: Vallongo },
   { name: "Moncana", href: "https://moncana.pl/", src: Moncana },
+];
+
+export const HERO_SLIDES: HeroSlideItem[] = [
+  {
+    title: "Zdrowo, smacznie, bezglutenowo!",
+    subtitle: "Przepisy, restauracje i porady dla osób na diecie bezglutenowej",
+    image: welcomeImg,
+    alt: "Restauracja Rola Pao w Warszawie",
+    links: [
+      { label: "Przepisy", href: "#sniadania" },
+      {
+        label: "Poradniki",
+        href: "#celiakia-podstawowe-informacje",
+        className: "white",
+      },
+    ],
+  },
+  {
+    title: "Mapa miejsc bezglutenowych",
+    subtitle:
+      "Restauracje, kawiarnie, cukiernie bez glutenu w Polsce i na świecie sprawdzone osobiście przeze mnie",
+    image: glutenFreeHeroMap,
+    alt: "Mapa miejsc bezglutenowych",
+    links: [{ label: "Sprawdź", href: "/bezglutenowe-miejsca" }],
+  },
+  {
+    title: "Bezglutenowe śniadania",
+    subtitle:
+      "Szukasz pomysłów na śniadania bezglutenowe, które są nie tylko zdrowe, ale i pyszne? Dobrze trafiłeś!",
+    image: breakfastImg,
+    alt: "Bezglutenowa śniadaniowa tortilla z komosy ryżowej",
+    reverse: true,
+    links: [{ label: "Sprawdź przepisy", href: "#sniadania" }],
+  },
+  {
+    title: "Bezglutenowe obiady",
+    subtitle:
+      "Szukasz inspiracji na obiady bezglutenowe, które będą nie tylko zdrowe, ale też naprawdę pyszne? Świetnie trafiłeś!",
+    image: dinnerImg,
+    alt: "Kurczako-burger Teriyaki",
+    links: [{ label: "Sprawdź przepisy", href: "#obiady" }],
+  },
+  {
+    title: "Bezglutenowe desery",
+    subtitle:
+      "Marzysz o pysznych deserach bezglutenowych, które zachwycą smakiem, a przy tym będą lekkie i zdrowe? Jesteś we właściwym miejscu!",
+    image: dessertImg,
+    alt: "Wege sernik",
+    reverse: true,
+    links: [{ label: "Sprawdź przepisy", href: "#desery" }],
+  },
+  {
+    title: "Bezglutenowe restauracje w Polsce",
+    subtitle:
+      "Szukasz miejsc, gdzie możesz zjeść pysznie i bez glutenu w Polsce? Oto moje rekomendacje!",
+    image: saskaSushiWarszawa,
+    alt: "Saska Sushi",
+    links: [{ label: "Sprawdź", href: "#restauracje-polska" }],
+  },
+  {
+    title: "Bezglutenowe restauracje na świecie",
+    subtitle:
+      "Szukasz miejsc, gdzie możesz zjeść pysznie i bez glutenu na świecie? Oto moje rekomendacje!",
+    image: pastaGlutenFreeRoma,
+    alt: "Restauracja Mama Eat, Włochy",
+    reverse: true,
+    links: [{ label: "Sprawdź", href: "#restauracje-europa" }],
+  },
+  {
+    title: 'Bezglutenowe produkty - "Czy to ma gluten?"',
+    subtitle:
+      "Sprawdzone produkty bezglutenowe, które warto mieć w swojej kuchni i w codziennym życiu",
+    image: pierogi,
+    alt: "Czy to ma gluten?",
+    links: [{ label: "Sprawdź", href: "#produkty" }],
+  },
+  {
+    title: "Bezglutenowe wydarzenia",
+    subtitle:
+      "Bezglutenowe eventy, warsztaty i spotkania dla osób na diecie bezglutenowej",
+    image: bezglutenoweHalloween,
+    alt: "Bezglutenowe wydarzenia",
+    links: [{ label: "Sprawdź", href: "#wydarzenia" }],
+  },
 ];
