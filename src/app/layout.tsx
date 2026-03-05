e7322e264aa0834ea4cbb71fd726a46c63b5d842
goldipl <mgodlewski92@gmail.com>
2026-03-05 16:19:06 +0100

    feat: update SEO metadata and remove legacy document file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,57 @@ import type { Metadata } from "next";
 import "@/sass/main.scss";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://bezglutenowakarola.pl"),
   title:
     "Bezglutenowa Karola - Przepisy Gluten Free | Celiakia - Karolina Wontorska",
   description:
-    "Bezglutenowa Karola - przepisy na dania bez glutenu, bez cukru, bez nabiału, idealne dla osób z celiakią.",
+    "Bezglutenowa Karola: Odkryj proste i pyszne przepisy bez glutenu, cukru i nabiału. Idealne dla osób z celiakią i na diecie eliminacyjnej. Gotuj zdrowo z Karoliną!",
+  keywords: [
+    "przepisy bezglutenowe",
+    "dieta bezglutenowa",
+    "celiakia",
+    "bez cukru",
+    "bez nabiału",
+    "Karolina Wontorska",
+    "bezglutenowe",
+    "gluten free",
+    "przepisy bez glutenu",
+    "zdrowe jedzenie",
+  ],
+  authors: [{ name: "Bezglutenowa Karola" }],
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  openGraph: {
+    type: "website",
+    locale: "pl_PL",
+    url: "https://bezglutenowakarola.pl",
+    title:
+      "Bezglutenowa Karola - Przepisy Gluten Free | Celiakia - Karolina Wontorska",
+    description:
+      "Znajdź przepisy na dania bez glutenu, bez cukru, bez nabiału, idealne dla osób z celiakią. Zdrowe i smaczne jedzenie dla osób na diecie bezglutenowej.",
+    images: [
+      {
+        url: "https://bezglutenowakarola.pl/_next/static/media/bezglutenowakarola-logo.3e6eebf5.svg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title:
+      "Bezglutenowa Karola - Przepisy Gluten Free | Celiakia - Karolina Wontorska",
+    description:
+      "Przepisy bez glutenu, bez cukru, bez nabiału, stworzone z myślą o osobach z celiakią i na diecie bezglutenowej. Sprawdź zdrowe inspiracje kulinarne.",
+    images: [
+      "https://bezglutenowakarola.pl/_next/static/media/bezglutenowakarola-logo.3e6eebf5.svg",
+    ],
+  },
+  verification: {
+    google: "Wwdgti5L7WUD354p16SJSkp1xWEkAU95jfBNILTVeFc",
+  },
 };
 
 export default function RootLayout({
