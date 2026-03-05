be9c9fae262f18bec7e27339f05362c364b297ee
goldipl <mgodlewski92@gmail.com>
2026-03-05 16:05:56 +0100

    fix: restore SEO metadata for improved search visibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import "@/sass/main.scss";
 
 export const metadata: Metadata = {
-  // title:
-  //   "Bezglutenowa Karola - Przepisy Gluten Free | Celiakia - Karolina Wontorska",
-  // description:
-  //   "Bezglutenowa Karola - przepisy na dania bez glutenu, bez cukru, bez nabiału, idealne dla osób z celiakią.",
+  title:
+    "Bezglutenowa Karola - Przepisy Gluten Free | Celiakia - Karolina Wontorska",
+  description:
+    "Bezglutenowa Karola - przepisy na dania bez glutenu, bez cukru, bez nabiału, idealne dla osób z celiakią.",
 };
 
 export default function RootLayout({
